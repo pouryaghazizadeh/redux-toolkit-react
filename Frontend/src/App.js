b76609eed4100d3d75fa2f7b812b8dd6da56b89a
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-12-24 17:20:29 +0100

    login signin and navbar component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,18 @@
 // tools
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 // component
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Todo from "./pages/todo/Todo";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todo" element={<Todo />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ Frontend/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+   
+    return (
+      <div>
+        <Link to="/" className={{fontSize:"x-large"}}>
+          Home
+        </Link>
+        <Link to="/todo" className={{}}>
+          Todo
+        </Link>
+        <Link to="/login" className={{}}>
+          Login
+        </Link>
+        <Link>Register</Link>
+      </div>
+    );
+}
+
+export default Navbar
